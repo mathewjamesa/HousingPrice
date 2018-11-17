cd969b8cf59932a231fe587d43f0811487e712fa
mikhailry <mikhail.rybalchenko@gmail.com>
2018-11-16 18:37:45 -0600

    Split data into testData/trainData. Saved in data folder.

---
+++ sampling.R
@@ -0,0 +1,17 @@
+#Split data into train/test datasets
+
+#install.packages('caret') #uncomment to install
+library('caret')
+
+#load data
+load('data_model.Rdata')
+
+#setting seed
+set.seed(11162018)
+
+#Doing stratified sampling
+trainInd <- createDataPartition(data_model_f$PRICE, p = 0.8, list = FALSE)
+trainData <- data_model_f[trainInd,]
+testData <- data_model_f[-trainInd,]
+stopifnot(nrow(trainData) + nrow(testData) == nrow(data_model_f))
+
